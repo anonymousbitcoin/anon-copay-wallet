9ea2d1c6c334f3d6cacafa205fc9c5c760eca302
Christopher Arguello <christopheraarguello@gmail.com>
2018-11-29 12:37:52 -0500

    Show network stats in fullnode

--- src/js/services/networkStatsService.js
+++ src/js/services/networkStatsService.js
@@ -1,27 +1,21 @@
 'use strict';
 
-angular.module('copayApp.services').factory('networkStatsService', function($log, bitcoreAnon, storageService, lodash, profileService) {
-  var root = {};
-  root.getInfo = function(cb) {
-    let result = []
-    fetch("https://explorer.anonfork.io/insight-api-anon/status?q=getinfo", 
-        { headers: 
-            { "Content-Type": 
-                "application/json; 
-                charset=utf-8" 
-            }
-        })
-    .then(res => res.json()) // parse response as JSON (can be res.text() for plain response)
-    .then(response => {
-      result = response
-        // here you do what you want with response
-        return cb(null, result);
-    })
-    .catch(err => {
-        console.log("sorry, there are no results for your search")
-	});
-    return root;
-    };
-})   
-;
+angular.module('copayApp.services').service('networkStatsService', function($log, bitcoreAnon, storageService, lodash, profileService) {
+    
+    // var root = {};
+    
+    this.getInfo = function(cb) {
+        var https = require('https');
+            https.get('https://explorer.anonfork.io/insight-api-anon/status?q=getinfo', function(err, res) {
+                if(err){
+                    return error(error, "There's been an error")
+                }
+                else{
+                    console.log(res)
+                    return cb(res)
+                }
+            });
+        };
+});   
+
 
